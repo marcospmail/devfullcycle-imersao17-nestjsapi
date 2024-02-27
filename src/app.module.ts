81f5713ca6f4972085f3fd77ceafe0dad57ffd0e
Marcos Paulo <marcosp.mail@gmail.com>
2024-02-26 23:13:56 -0300

    Project setup

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ProductsModule } from './products/products.module'
+import { Product } from './products/entities/product.entity'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Product],
+      synchronize: true,
+      logging: true,
+    }),
+    ProductsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
